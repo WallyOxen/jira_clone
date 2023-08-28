97b5bb691a8ee0c6b9a4208f4f7233254d429dda
WallyOxen <wally@wallys.world>
2023-08-28 19:09:19 -0400

    Stage 1 step 1 setup

---
+++ src/models.rs
@@ -0,0 +1,27 @@
+pub enum Status {
+  // TODO: add fields
+}
+
+pub struct Epic {
+  // TODO: add fields
+}
+
+impl Epic {
+  pub fn new(name: String, description: String) -> Self {
+    todo!()
+  }
+}
+
+pub struct Story {
+  // TODO: add fields
+}
+
+impl Story {
+  pub fn new(name: String, description: String) -> Self {
+    todo!()
+  }
+}
+
+pub struct DBState {
+  // TODO: add fields
+}
